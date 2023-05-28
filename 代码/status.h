b86d086b66a371312cd202ad530c51fcf0f64171
liYuHang <innovation_lyh@163.com>
2023-05-28 22:42:21 +0800

    2023-5-28 晚-雨天

---
+++ 代码/status.h
@@ -0,0 +1,11 @@
+
+// 结果状态代码
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+// Status 函数类型，函数结果状态代码
+typedef int Status;
+typedef char ElemType;
